e6f75fe0f984be6e1f81be5df87f7c937613ee4d
HuzaifaIOSTraning <muhammad.huzaifa@emagportal.com>
2025-08-26 09:56:10 +0500

    Initial Commit

---
+++ Todo_List/Todo_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  Todo_ListApp.swift
+//  Todo_List
+//
+//  Created by Huzaifa on 26/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct Todo_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Todo_ListTests/Todo_ListTests.swift
@@ -0,0 +1,17 @@
+//
+//  Todo_ListTests.swift
+//  Todo_ListTests
+//
+//  Created by Huzaifa on 26/08/2025.
+//
+
+import Testing
+@testable import Todo_List
+
+struct Todo_ListTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
